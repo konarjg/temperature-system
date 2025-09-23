fcb4b890dfd85020e56abd0a7b3cc536d47e3627
konarjg <konarskikrzysztof1@gmail.com>
2025-09-24 00:57:08 +0200

    Fixed line endings

--- TemperatureSystem/setup.sh
+++ TemperatureSystem/setup.sh
@@ -3,5 +3,6 @@ set -e
 
 echo "Running EF Core database migrations..."
 dotnet ef database update --project DatabaseAdapters --startup-project TemperatureSystem --context SqLiteDatabaseContext
+echo "Done migrating the database"
 
 exec "$@"
