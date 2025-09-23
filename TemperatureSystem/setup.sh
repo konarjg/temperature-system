0bcea0d412c43998a39d998db2986a1787ea2887
konarjg <konarskikrzysztof1@gmail.com>
2025-09-24 01:23:24 +0200

    Removed BOM from setup script to fix it in Docker

--- TemperatureSystem/setup.sh
+++ TemperatureSystem/setup.sh
@@ -1,8 +1,7 @@
-﻿#!/bin/sh
+#!/bin/sh
 set -e
 
 echo "Running EF Core database migrations..."
 dotnet ef database update --project DatabaseAdapters --startup-project TemperatureSystem --context SqLiteDatabaseContext
-echo "Done migrating the database"
 
 exec "$@"
