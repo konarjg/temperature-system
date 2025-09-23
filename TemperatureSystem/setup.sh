5bcfbc7dff5fb39a21af67368e71499e063b8291
konarjg <konarskikrzysztof1@gmail.com>
2025-09-24 00:30:12 +0200

    Fixed setup script path in Docker

---
+++ TemperatureSystem/setup.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+set -e
+
+echo "Running EF Core database migrations..."
+dotnet ef database update --project DatabaseAdapters --startup-project TemperatureSystem --context SqLiteDatabaseContext
+
+exec "$@"
